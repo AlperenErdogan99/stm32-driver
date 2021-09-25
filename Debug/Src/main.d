3ef0c51bb2a7585bab1d07163883774e1e80186e
Alperen Erdoğan <falperenerdogan998@gmail.com>
2021-09-25 17:28:59 +0300

    add base addresses of peripherals

---
+++ Debug/Src/main.d
@@ -0,0 +1,4 @@
+Src/main.o: ../Src/main.c \
+ C:/Users/alperen/Desktop/stm32-workspace/stm32-driver/drivers/Inc/stm32f3xx.h
+
+C:/Users/alperen/Desktop/stm32-workspace/stm32-driver/drivers/Inc/stm32f3xx.h:
