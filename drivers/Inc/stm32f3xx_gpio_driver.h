4738c2d8d021641b121e027f819db4b20b8d9654
Alperen Erdoğan <falperenerdogan998@gmail.com>
2021-09-27 10:14:51 +0300

    create gpio driver header and source files

---
+++ drivers/Inc/stm32f3xx_gpio_driver.h
@@ -0,0 +1,14 @@
+/*
+ * stm32f3xx_gpio_driver.h
+ *
+ *  Created on: Sep 27, 2021
+ *      Author: alperen
+ */
+
+#ifndef INC_STM32F3XX_GPIO_DRIVER_H_
+#define INC_STM32F3XX_GPIO_DRIVER_H_
+
+#include "stm32f3xx.h"
+
+
+#endif /* INC_STM32F3XX_GPIO_DRIVER_H_ */
---
+++ drivers/Src/stm32f3xx_gpio_driver.c
@@ -0,0 +1,10 @@
+/*
+ * stm32f3xx_gpio_driver.c
+ *
+ *  Created on: Sep 27, 2021
+ *      Author: alperen
+ */
+#include "stm32f3xx_gpio_driver.h"
+
+
+
