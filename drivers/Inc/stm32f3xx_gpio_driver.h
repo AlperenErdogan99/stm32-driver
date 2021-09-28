e9175a60dc0a5dd8776c256fe32cdb6cc4d25859
Alperen Erdoğan <falperenerdogan998@gmail.com>
2021-09-28 08:54:15 +0300

    create handle structure for a GPIO pin, create configuration structure for a GPIO pin

--- drivers/Inc/stm32f3xx_gpio_driver.h
+++ drivers/Inc/stm32f3xx_gpio_driver.h
@@ -9,6 +9,26 @@
 #define INC_STM32F3XX_GPIO_DRIVER_H_
 
 #include "stm32f3xx.h"
+/*
+ * This is a configuration structure for a GPIO pin
+ */
+typedef struct{
+	uint8_t GPIO_PinNumber;
+	uint8_t GPIO_PinMode;
+	uint8_t GPIO_PinSpeed;
+	uint8_t GPIO_PinPuPdControl;
+	uint8_t GPIO_PinOpType;
+	uint8_t GPIO_PinAltFunMode;
+}GPIO_PinConfig_t;
 
 
+/*
+ * This is a Handle structure for a GPIO pin
+ */
+typedef struct{
+
+	GPIO_RegDef_t *pGPIOx; /* This holds the  base address of the GPIO port which the pin belongs*/
+	GPIO_PinConfig_t GPIO_PinConfig; /* This holds GPIO pin configuration settings */
+
+}GPIO_Handle_t;
 #endif /* INC_STM32F3XX_GPIO_DRIVER_H_ */
