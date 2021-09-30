35e138fee5600fdc29249e9824b2ad936341dd7c
Alperen Erdoğan <falperenerdogan998@gmail.com>
2021-09-30 13:39:41 +0300

    add gpio input output configuration to 001led_toggle test code

--- Src/001led_toggle.c
+++ Src/001led_toggle.c
@@ -16,6 +16,7 @@ int main(void){
 	GPIO_Handle_t GpioLed ;
 	GpioLed.pGPIOx = GPIOA;
 	GpioLed.GPIO_PinConfig.GPIO_PinNumber = GPIO_PIN_NO_5;
+	GpioLed.GPIO_PinConfig.GPIO_PinMode = GPIO_MODE_OUT;
 	GpioLed.GPIO_PinConfig.GPIO_PinSpeed = GPIO_SPEED_FAST;
 	GpioLed.GPIO_PinConfig.GPIO_PinOpType = GPIO_OP_TYPE_PP;
 	GpioLed.GPIO_PinConfig.GPIO_PinPuPdControl = GPIO_NO_PUPD;
